0af92c6d90bf05f068311429d4b9fe7ce38d18b6
JPS Archives <joydipbanik6@gmail.com>
2023-08-19 15:42:23 +0530

    update font color

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+// reuseable function
 function changeValue(elementId, styleAttribute, value) {
   document.getElementById(elementId).addEventListener("click", function () {
     const textarea = document.getElementById("text-area");
     if (textarea.style[styleAttribute] === value) {
-      textarea.style[styleAttribute] = ""; // Remove the style
+      textarea.style[styleAttribute] = "";
     } else {
-      textarea.style[styleAttribute] = value; // Apply the style
+      textarea.style[styleAttribute] = value;
     }
   });
   return changeValue;
@@ -14,29 +15,28 @@ const fontWeight = changeValue("bold", "fontWeight", "bold");
 const fontItalic = changeValue("italic", "fontStyle", "italic");
 const fontUnderLine = changeValue("underline", "textDecoration", "underline");
 
-
 const textLeft = changeValue("text-left", "textAlign", "left");
 const textCenter = changeValue("text-center", "textAlign", "center");
 const textRight = changeValue("text-right", "textAlign", "right");
 const textJustify = changeValue("text-justify", "textAlign", "justify");
 
-const textCapitalize = changeValue(
-  "text-capitalize",
-  "textTransform",
-  "capitalize"
-);
+const textCapitalize = changeValue("text-capitalize","textTransform","capitalize");
 
 const fontSize = document.getElementById("font-size");
-fontSize.addEventListener('change', function () {
-    const size = fontSize.value;
-    const fontSizeNumber = parseFloat(size);
-    // console.log(fontSizeNumber);
-    const check = changeValue("font-size", "fontSize", `${fontSizeNumber}`);
-    console.log(check);
-})
+fontSize.addEventListener("change", function () {
+  const size = fontSize.value;
+  const fontSizeNumber = parseFloat(size);
+  const check = changeValue("font-size", "fontSize", `${fontSizeNumber}px`);
+  console.log(check);
+});
 
- const textColor = document.getElementById("font-color");
- textColor.addEventListener("input", function () {
-   const selectedColorValue = textColor.value;
-     changeValue("font-color","color",`${selectedColorValue}`);
- });
+document.getElementById("font-color").addEventListener("input", function () {
+  const textarea = document.getElementById("text-area");
+  const textColor = document.getElementById("font-color").value;
+
+  if (textarea.style.color === textColor) {
+    textarea.style.color = "";
+  } else {
+    textarea.style.color = textColor;
+  }
+});
